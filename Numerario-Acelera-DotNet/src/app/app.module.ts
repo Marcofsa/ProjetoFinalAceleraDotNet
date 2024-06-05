e5cfca84b46581a45f716693af69f188eee8cf25
Marcos Antonio Alves de Sá Filho <garenmanjador@hotmail.com>
2024-06-04 22:41:13 -0300

    Author: Marcos de Sá Filho Date: 04/06/2024 Description, Criação do componente de saldos e alguns ajustes gerais

--- Numerario-Acelera-DotNet/src/app/app.module.ts
+++ Numerario-Acelera-DotNet/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BalanceComponent } from './components/balance/balance.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, DashboardComponent],
+  declarations: [AppComponent, LoginComponent, DashboardComponent, BalanceComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],

e88c0ad79e7988915c69c0cf1dd9f011ad4ce6f1
samarcosdev <samarcosdev@gmail.com>
2024-06-05 15:22:11 +0000

    Testes

--- Numerario-Acelera-DotNet/src/app/app-routing.module.ts
+++ Numerario-Acelera-DotNet/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { BalanceComponent } from './components/balance/balance.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Login', component: LoginComponent},
+  {path: 'Balance', component: BalanceComponent},
+  {path: 'Dashboard', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

257e3ca0199e0a0c028c4b7ceb72fee17aff341a
Marcos Antonio Alves de Sá Filho <garenmanjador@hotmail.com>
2024-06-09 11:07:11 -0300

    Comentando o arquivo do excel.

--- Numerario-Acelera-DotNet/src/app/utils/excel.util.ts
+++ Numerario-Acelera-DotNet/src/app/utils/excel.util.ts
@@ -1,19 +1,19 @@
-import * as XLSX from 'xlsx';
+// import * as XLSX from 'xlsx';
 
-// Define a função que cria o excel
-function createExcelReport(data: any[]) {
-  const header = [[], [], [], [], [], [], [], [], []];
+// // Define a função que cria o excel
+// function createExcelReport(data: any[]) {
+//   const header = [[], [], [], [], [], [], [], [], []];
 
-  const sheetData = [...header, ...data];
+//   const sheetData = [...header, ...data];
 
-  const newBook = XLSX.utils.book_new();
-  const toSheetData = XLSX.utils.aoa_to_sheet(sheetData);
+//   const newBook = XLSX.utils.book_new();
+//   const toSheetData = XLSX.utils.aoa_to_sheet(sheetData);
 
-  XLSX.utils.book_append_sheet(newBook, toSheetData, 'Report');
+//   XLSX.utils.book_append_sheet(newBook, toSheetData, 'Report');
 
-  XLSX.writeFile(newBook, 'Relatório.xlsx');
-}
+//   XLSX.writeFile(newBook, 'Relatório.xlsx');
+// }
 
-const exampleData = [[], [], [], [], [], [], [], [], [], [], [], [], [], []];
+// const exampleData = [[], [], [], [], [], [], [], [], [], [], [], [], [], []];
 
-createExcelReport(exampleData);
+// createExcelReport(exampleData);

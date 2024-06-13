85b145132f57e77b62c45f8089a04e6a7bc237ab
samarcosdev <samarcosdev@gmail.com>
2024-06-13 12:40:19 +0000

    Author: Marcos de Sá Filho Date13/06/2024 Description: Funcional.

--- Numerario-Acelera-DotNet/src/app/components/dashboard/dashboard.component.ts
+++ Numerario-Acelera-DotNet/src/app/components/dashboard/dashboard.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss',
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-  
+  transacoes = [
+    {
+      data: '2024-06-13',
+      hora: '10:30',
+      agencia: '1234',
+      operacao: 'Débito',
+      situacao: 'Concluída',
+      valor: 150.75
+    },
+    {
+      data: '2024-06-12',
+      hora: '15:45',
+      agencia: '5678',
+      operacao: 'Crédito',
+      situacao: 'Pendente',
+      valor: 200.50
+    },
+    // Adicione quantas transações desejar
+  ];
 }
